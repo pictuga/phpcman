d707ce1633022eea52b233963b32242c8cf39417
pictuga <contact@pictuga.com>
2012-12-30 12:54:17 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,335 @@
+<?php
+include_once('libpacman.php');
+define("EOL", PHP_EOL);
+define("TAB", "\t");
+?>
+
+<html>
+<head>
+	<title>phpcman</title>
+	
+	<meta charset='UTF-8' />
+	
+	<meta name="description" content="See your Archlinux system updates right from your browser!" />
+	<meta name="author" content="pictuga" />
+	
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
+	
+	<!--
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+	
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+	<link rel="apple-touch-icon" href="logo.png" />
+	<link rel="apple-touch-startup-image" href="startup.png" />
+	-->
+	
+	<style type="text/css">
+		body *
+		{
+			padding: 0;
+			margin: 0;
+		}
+		
+		body
+		{
+			max-width: 100%;
+			background-color: white;
+			padding-bottom: 1em;
+		}
+		
+		h1
+		{
+			text-align: center;
+			font-weight: bold;
+			font-size: 2em;
+			color: green;
+			margin: 2em auto .5em auto;
+		}
+		
+		p
+		{
+			text-align: justify;
+			padding: 0;
+			margin: 0;
+			text-indent: 1em;
+		}
+		
+		#count > span
+		{
+			font-weight: bold;
+		}
+		
+		#list
+		{
+			margin-top: 1em;
+		}
+		
+		#list > div
+		{
+			border-radius: 10px;
+			border: 1px solid gray;
+			padding: 10px;
+			margin-bottom: .5em;
+			font-family: sans-serif;
+		}
+		
+		#list span.name
+		{
+			display: block;
+			min-width: 33%;
+			font-weight: bold;
+		}
+		
+		#list span.repo
+		{
+			float: right;
+			content: '[';
+			font-family: monospace;
+		}
+		
+		#list span.repo:before
+		{
+			content: '[';
+		}
+		
+		#list span.repo:after
+		{
+			content: ']';
+		}
+		
+		#list span.link, #list span.version
+		{
+			font-style: italic;
+		}
+		
+		#list span.link a, #list span.link a:visited
+		{
+			color: inherit;
+			text-decoration: inherit;
+		}
+		
+		#calc
+		{
+			text-align: center;
+			font-size: .5em;
+			font-family: sans-serif;
+		}
+		
+		#intro
+		{
+			padding-bottom: 1em;
+			margin-bottom: 1em;
+			border-bottom: 1px solid black;
+		}
+		
+		#intro form
+		{
+			margin: 1em 0;
+			text-align: center;
+		}
+		
+		#intro:after
+		{
+			position: relative;
+			bottom: -1.5em;
+			content: "Below, a demo.";
+			display: block;
+			color: blue;
+			text-align: center;
+			font-style: italic;
+			margin-bottom: .5em;
+		}
+		
+		form label
+		{
+			font-family: monospace;
+			font-weight: bold;
+		}
+		
+		form label:after
+		{
+			content: " : ";
+			font-weight: normal;
+		}
+		
+		form label:before
+		{
+			content: " $ ";
+			font-weight: normal;
+		}
+		
+		div.warning
+		{
+			background-color: pink;
+			border: 1px solid red;
+			padding: .5em;
+		}
+		
+		input[type=checkbox].toggle
+		{
+			display: none;
+		}
+		
+		input[type=checkbox].toggle + label
+		{
+			cursor: pointer;
+		}
+		
+		input[type=checkbox].toggle + label:before
+		{
+			content: "→";
+		}
+		
+		input[type=checkbox].toggle:checked + label:before
+		{
+			content: "↓";
+		}
+
+		input[type=checkbox].toggle + label + div
+		{
+			display: none;
+			border-left: 1px solid gray;
+			margin: 0 0 1em 1em;
+			padding-left: 1em;
+		}
+
+		input[type=checkbox].toggle:checked + label + div
+		{
+			display: block;
+		}
+		
+		#ad
+		{
+			padding: 0 !important;
+			margin: 0 !important;
+			position: absolute !important;
+			top: 0 !important;
+			left: 0 !important;
+		}
+		
+		@media screen and (min-width : 800px)
+		{
+			body
+			{
+				max-width : 50%;
+				margin : auto;
+			}
+		}
+	</style>
+</head>
+<body>
+
+	<h1>phpcman</h1>
+
+<?php
+main();
+
+function main()
+{
+	if(!isset($_GET['id']))
+		addNew();
+	
+	$id = (isset($_GET['id']) && is_numeric($_GET['id'])) ? $_GET['id'] : 0;
+	displayId($id);
+}
+
+function addNew()
+{
+	if(isset($_FILES['file'])
+	&& $_FILES['file']['error'] === 0
+	&& $_FILES['file']['size'] < 100000
+	&& explode('/', mime_content_type($_FILES['file']['tmp_name']))[0] == 'text')
+	{
+		$tmp = $_FILES['file']['tmp_name'];
+		
+		$diff = array_diff(getListFile($tmp), array_keys(getCoreInfo(getListFile($tmp))));
+		$aur = checkAUR($diff);
+		$list = kasort(array_merge($aur, getCoreInfo(getListFile($tmp))));
+		
+		$id = ((int) explode('.', array_slice(scandir('list'), -1)[0])[0] ) +1;
+		
+		file_put_contents('list/' . $id . '.list', implode("\n", array_keys($list)));
+		file_put_contents('list/' . $id . '.time', time());
+		
+		$depends = addDepends(getExtraInfo(getCoreInfo(getListFile($tmp))));
+		$list = kasort(array_merge($aur, $depends));
+		
+		file_put_contents('list/' . $id . '.cache', implode("\n", array_keys($list)));
+		
+		ob_clean();
+		header('Location: ' . $_SERVER['SCRIPT_URI'] . '?id=' . $id);
+	}
+	else
+	{
+		echo '<div id="intro">' . EOL;
+			echo '<p><i>What\'s this?</i> This website intends to show you your Archlinux system updates, so that you know whether
+			it\'s worth checking them at home. The main use-case is to check this website on your smartphone, 
+			at work, or anywhere else.</p>' . EOL;
+			echo '<p>Of course, you can add your own configuration! Result sample below! 
+			Therefore you have to upload a list of your installed packages to this website. That\'s all!</p>' . EOL;
+			echo '<p>The website actually shows installed packages ordered by "builddate" (or upload for AUR)(most recent up). 
+			Architectures: <a href="/phpcman/32bit/">32bit</a> or <a href="/phpcman/">64bit</a> or <a href="/phpcman/rpi/">rpi</a>.</p>' . EOL;
+			
+			echo '<form method="post" action="?" enctype="multipart/form-data">' . EOL;
+			echo TAB . '<input type="hidden" name="MAX_FILE_SIZE" value="50000" />' . EOL;
+			echo TAB . '<label for="file">pacman -Qqe > ~/tmp</label><input type="file" id="file" name="file">' . EOL;
+			echo TAB .  '<input type="submit" value="Add">' . EOL;
+			echo '</form>' . EOL;
+			
+			if(isset($_FILES['file']))
+				echo '<div class="warning">Looks like you tried to upload your package list, but that it failed. 
+				Please check you\'re uploading the right file. The list file is supposed to be small (&lt;100ko), 
+				any bigger file will be rejected.</div>' . EOL;
+		echo '</div>' . EOL;
+	}
+}
+
+function displayId($id)
+{
+	if(!is_file('list/' . $id . '.list') || !is_file('list/' . $id . '.cache') || !is_file('list/' . $id . '.time'))
+	{
+		echo '<div class="warning">The requested setup cannot be found. Try uploading a new package list.</div>';
+		return false;
+	}
+	
+	$START_TIME = microtime(true);
+	updateCache();
+	
+	$installed = getExtraInfo(getCoreInfo(getListFile('list/' . $id . '.cache')));
+	
+	$diff = array_diff(getListFile('list/' . $id . '.list'), array_keys(getCoreInfo(getListFile('list/' . $id . '.list'))));
+	$aur = checkAUR($diff);
+	$all = kasort(array_merge($aur, $installed));
+	
+	$installed = $all;
+	
+	$new = array_slice(listByBuild($installed), 0, 100);
+	
+	echo '<input type="checkbox" class="toggle" id="toggle" /><label for="toggle">More info</label>' . EOL;
+	echo '<div>' . EOL;
+		echo TAB . '<p>You have <b>' . count($installed) . '</b> packages installed including dependencies.</p>';
+		echo TAB . '<p>Your personal result page is <b>' . $_SERVER['SCRIPT_URI'] . '?id=' . $id . '</b>.
+		You can access it from any browser, any time you want. You can find a nice QRCode to quickly access this page 
+		<a href="https://www.wolframalpha.com/input/?i=qrcode+' . $_SERVER['SCRIPT_URI'] . '?id=' . $id . '">here</a>.</p>' . EOL;
+	echo '</div>' . EOL;
+	
+	echo '<div id="list">' . EOL;
+	
+	foreach($new as $package)
+	{
+		echo '<div>' . EOL;
+			echo TAB . '<span class="repo">' . $package['repo'] . '</span>' . EOL;
+			echo TAB . '<span class="name">' . $package['name'] . '</span>' . EOL;
+			echo TAB . '<span class="date">' . date('Y/m/d H:i', $package['desc']['BUILDDATE']) . '</span>' . EOL;
+			echo TAB . '<span class="version">' . $package['version'] . '</span>' .EOL;
+			echo TAB . '<span class="link"><a href="' . $package['page'] . '">(…)</a></span>' . EOL;
+		echo '</div>' . EOL;
+	}
+	
+	echo '</div>' . EOL;
+	
+	echo '<div id="calc">⌚ ' . (microtime(true) - $START_TIME) . '</div>' . EOL;
+}
+?>
+</body>
---
+++ libpacman.php
@@ -0,0 +1,298 @@
+<?php
+//init
+
+chdir(dirname(__FILE__));
+safetyCheck();
+parseConf();
+
+//rand tools
+
+function makeSimpleAssoc($array)
+{
+	$out = array();
+	foreach($array as $key => $value)
+		$out[$key] = $value[0];
+	return $out;
+}
+
+function safetyCheck()
+{
+	if(is_file('cache_lock'))
+	{
+		if(file_get_contents('cache_lock') > time()+60*10)
+		{
+			echo "update failed...";
+			unlink('cache_lock');
+			updateCache();
+		}
+		else
+		{
+			echo "updating db...";
+			exit;
+		}
+	}
+	return true;
+}
+
+function kasort($array)
+{
+	ksort($array);
+	return $array;
+}
+
+//file tools
+
+function ddlFile($url, $path=null, $etag=null)
+{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	if($etag !== null && is_file($etag))
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('If-None-Match: ' . file_get_contents($etag)));
+	$data = curl_exec($ch);
+	
+	$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+	$header = substr($data, 0, $header_size);
+	$body = substr($data, $header_size);
+	
+	$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	
+	if($etag !== null)
+	{
+		preg_match_all('#"[0-9a-z-]{6,}"#', $header, $match);
+		file_put_contents($etag, $match[0][0]);
+	}
+	
+	if($http_code == 304)
+		return "unchanged";
+	
+	if($path != null && $http_code == 200)
+		return file_put_contents($path, $body);
+	
+	if($path == null)
+		return $body;
+}
+
+//conf
+
+function parseConf($file='settings.ini')
+{
+	$GLOBALS['CONF'] = @parse_ini_file($file, true);
+	$GLOBALS['PACCONF'] = @parse_ini_file($GLOBALS['CONF']['server']['pacconf'], true);
+	unset($GLOBALS['PACCONF']['options']);
+	
+	foreach($GLOBALS['PACCONF'] as $name => $repo)
+		$GLOBALS['PACCONF'][$name]['url'] = parseRepoUrl($name, $GLOBALS['CONF']);
+}
+
+//repo tools
+
+function parseRepoUrl($name)
+{
+	$url = $GLOBALS['CONF']['server']['url'];
+	$url = str_replace('$repo', $name, $url);
+	$url = str_replace('$arch', $GLOBALS['CONF']['server']['arch'], $url);
+	
+	return $url;
+}
+
+function updateCache()
+{
+	file_put_contents('cache_lock', time());
+	
+	foreach($GLOBALS['PACCONF'] as $name => $repo)
+	{
+		$url	= $repo['url'] . '/' . $name . '.db';
+		$dest	= $GLOBALS['CONF']['cache']['path'] . $name . '.db';
+		$etag	= $GLOBALS['CONF']['cache']['path'] . $name . '.etag';
+		
+		if(!is_dir($GLOBALS['CONF']['cache']['path']))
+			mkdir($GLOBALS['CONF']['cache']['path']);
+		
+		ddlFile($url, $dest, $etag);
+	}
+	
+	unlink('cache_lock');
+}
+
+function listRepoPackage($name)
+{
+	$dir	= 'phar://' . $GLOBALS['CONF']['cache']['path'] . $name . '.db';
+	$list	= scandir($dir);
+		//unset($list[0]); unset($list[1]);//only for real fs
+	$out	= array();
+	
+	foreach($list as $file)
+	{
+		preg_match_all('#^(.+)-([0-9a-zA-Z_.:~]+-[0-9.]+)$#' , $file, $match);
+		$page = 'https://www.archlinux.org/packages/' . $name . '/' . $GLOBALS['CONF']['server']['arch'] . '/' . $match[1][0] . '/';
+		$out[$match[1][0]] = array(
+			'name'	=> $match[1][0],
+			'repo'	=> $name,
+			'version' => $match[2][0],
+			'file'	=> $match[0][0],
+			'page'	=> $page);
+	}
+	
+	//print_r($out);
+	return $out;
+}
+
+function listPackagesByRepo()
+{
+	static $cache = 0;
+	if(!$cache)
+	{
+		$cache = array();
+		foreach($GLOBALS['PACCONF'] as $name => $repo)
+			$cache[$name] = listRepoPackage($name);
+	}
+	
+	return $cache;
+}
+
+function listPackages()
+{
+	static $cache = 0;
+	if(!$cache)
+		$cache = call_user_func_array('array_merge', listPackagesByRepo());
+	return $cache;
+}
+
+function getExtraInfo($list, $batch=true)
+{
+	if(!$batch)
+		$list = array($list);
+	
+	foreach($list as &$package)
+	{
+		if(count($package) == 5)
+		{
+			$package['depends'] = parsePackageInfo($package['file'], $package['repo'], 'depends');
+			$package['desc'] = makeSimpleAssoc(parsePackageInfo($package['file'], $package['repo'], 'desc'));
+		}
+	}
+	
+	return $batch ? $list : $list[0];
+}
+
+
+//aur tools
+
+function checkAUR($list)
+{
+	if(!count($list))
+		return;
+	
+	$url = "https://aur.archlinux.org/rpc.php?type=multiinfo&arg[]=";
+	$args = implode('&arg[]=', $list);
+	$url = $url . $args;
+	
+	$aur = json_decode(ddlFile($url), true)['results'];
+	
+	$out = array();
+	foreach($aur as $package)
+	{
+		$out[$package['Name']] = array(
+			'name'	=> $package['Name'],
+			'version' => $package['Version'],
+			'repo'	=> 'aur',
+			'desc'	=> array('BUILDDATE' => $package['LastModified']),
+			'page'	=> 'https://aur.archlinux.org/packages/' . $package['Name'] . '/'
+			);
+	}
+	
+	return $out;
+}
+
+//package tools
+
+function parsePackageInfo($file, $repo, $info)
+{
+	//$file = $GLOBALS['CONF']['cache']['path'] . $repo . '.out/' . $file . '/' . $info;
+	$file = 'phar://' . $GLOBALS['CONF']['cache']['path'] . $repo . '.db/' . $file . '/' . $info;
+	
+	$out = array();
+	$cur = "";
+	
+	$handle = @fopen($file, "r");
+	if ($handle)
+	{
+		while (($line = fgets($handle, 4096)) !== false)
+			if($line != "\n")
+			{
+				$line = rtrim($line, "\r\n");
+				
+				if($line[0] == '%' && substr($line, -1) == "%")
+				{
+					$cur = substr($line, 1, -1);
+					$out[$cur] = array();
+				}
+				else
+				{
+					$out[$cur][] = $line;
+				}
+			}
+		if (!feof($handle))
+			return false;
+		fclose($handle);
+	}
+	else	return false;
+	
+	return $out;
+}
+
+function addDepends($list)
+{
+	while(list($name, $package) = each($list)) 
+	{
+		if(array_key_exists('DEPENDS', $package['depends']))
+		{
+			foreach($package['depends']['DEPENDS'] as $depend)
+			{
+				$depend = strtok($depend,'>=<');
+				if(!array_key_exists($depend, $list))
+				{
+					if(array_key_exists($depend, listPackages()))
+						$list[$depend] = getExtraInfo(listPackages()[$depend], false);
+				}
+			}
+		}
+	}
+	
+	return $list;
+}
+
+//installed info
+
+function getListFile($file)
+{
+	return explode("\n", rtrim(file_get_contents($file), "\r\n"));
+}
+
+function getCoreInfo($list)
+{
+	$byrepo = listPackages();
+	$out = array();
+	
+	foreach($list as $package)
+	{
+		if(array_key_exists($package, $byrepo))
+			$out[$package] = $byrepo[$package];
+	}
+	
+	return $out;
+}
+
+function listByBuild($list)
+{
+	//losing keys
+	usort($list, function($a, $b)
+		{
+			return $a['desc']['BUILDDATE'] - $b['desc']['BUILDDATE'];
+		});
+	return array_reverse($list);
+}
+?>
